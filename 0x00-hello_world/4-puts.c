2dbd68c4aaa7ad952e17f4b51179673fe9823171
peremaxpere <peremaxpere@gmail.com>
2022-12-10 05:26:53 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
 /**
- * main Entry
- * Return: Always 0
+ * main -a c program tha prints exactly "Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
 	puts("/"Programming is like building a multi ligual puzzle);
 	return (0);
-}	
+i}	
